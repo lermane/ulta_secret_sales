7f28862ef296e9f18ecdce7f4d2dbc75a8121a8e
Emily Lerman <emilyjlerman@gmail.com>
2020-07-23 14:24:26 -0400

    removed the 'prints' in the update_secret_sales.py file bc they're not necessary anymore

--- update_secret_sales.py
+++ update_secret_sales.py
@@ -8,8 +8,6 @@ import google_sheets_credentials as creds
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-print("\nstarting...\n")
-
 session = requests.Session()
 all_url_info = {}
 products = {}
@@ -17,7 +15,6 @@ products = {}
 all_url_info = ulta.get_url_dict(session)
 urls = list(all_url_info.keys())
 
-print('scraping ulta...')
 #I'm using threading to make the code run faster
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     futures = {executor.submit(ulta.scrape_url, url, session, products, all_url_info): url for url in urls}
@@ -32,7 +29,6 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 
 session.close()
 
-print('data cleaning...')
 #cleaning the data
 #df of every product on ulta's website
 ulta_df = pd.DataFrame.from_dict(products).transpose()
@@ -46,19 +42,17 @@ drop_na_options = pd.DataFrame.to_dict(secret_sales_df.dropna(subset=['options']
 #instead but I didn't feel like it
 secret_sales = pd.DataFrame.to_dict(secret_sales_df.transpose())
 
-print('getting available options...')
 #starting a headless selenium driver
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 driver = webdriver.Chrome(r'C:\Users\elerm\Downloads\chromedriver_win32\chromedriver.exe', options = chrome_options)
 products_in_stock = {}
 
-products_in_stock, secret_sales = ulta.get_products_in_stock(drop_na_options, secret_sales, driver)
+products_in_stock, secret_sales = ulta.get_products_in_stock(secret_sales, driver)
 
 driver.close()
 driver.quit()
 
-print('more data cleaning...')
 products_in_stock_df = pd.DataFrame.from_dict(products_in_stock).transpose().reset_index().rename(columns={'index' : 'name'})
 products_in_stock_df = pd.melt(products_in_stock_df, id_vars=['name'], var_name='price2', value_name='options2').dropna().set_index('name')
 secret_sales_df = pd.DataFrame.from_dict(secret_sales).transpose()
@@ -71,14 +65,12 @@ secret_sales_in_stock['price'] = pd.to_numeric(secret_sales_in_stock['price'])
 #final dataframe!!! yay!!!!!
 df = secret_sales_in_stock[['main_category', 'sub_category', 'sub_sub_category', 'name', 'brand', 'product', 'price', 'options', 'offers', 'rating', 'number_of_reviews', 'url']].fillna(' ')
 
-print("updating sheet hosted on mod's google drive...")
 #update the sheet hosted on the mod's google drive
 gapi.Create_Service(creds.get_credentials_file('main_mod'), creds.get_token_write_file('main_mod'), 'sheets', 'v4', ['https://www.googleapis.com/auth/spreadsheets'])
 gapi.Clear_Sheet(creds.get_sheet_id('main_mod'))
 gapi.Export_Data_To_Sheets(creds.get_sheet_id('main_mod'), df)
 gapi.Update_Filter(creds.get_sheet_id('main_mod'), creds.get_filter_id('main_mod'), len(df), len(df.columns))
 
-print('updating sheet hosted on my google drive...')
 #update the sheet hosted on my google drive
 gapi.Create_Service(creds.get_credentials_file('main_local'), creds.get_token_write_file('main_local'), 'sheets', 'v4', ['https://www.googleapis.com/auth/spreadsheets'])
 gapi.Clear_Sheet(creds.get_sheet_id('main_local'))
@@ -86,7 +78,5 @@ gapi.Export_Data_To_Sheets(creds.get_sheet_id('main_local'), df)
 gapi.Update_Filter(creds.get_sheet_id('main_local'), creds.get_filter_id('main_local'), len(df), len(df.columns))
 
 #saving it as a .csv for funsies/ just in case idk
-secret_sales_in_stock.to_csv('data/secret_sales_in_stock.csv')
-ulta_df.to_csv('data/ulta_df.csv')
-
-print('DONE')
+#secret_sales_in_stock.to_csv('data/secret_sales_in_stock.csv')
+#ulta_df.to_csv('data/ulta_df.csv')
